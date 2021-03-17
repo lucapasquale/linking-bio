47e59a452aa91ab2d1db51f0d88b0178c3780346
Luca Pasquale <lucapasquale.094@gmail.com>
2021-03-17 11:47:21 -0300

    bump all deps (#8)
    * bump alldeps
    
    * tsconfig
    
    * fixed backend lint

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,3 +1,4 @@
+import { join } from 'path'
 import { Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
@@ -16,7 +17,7 @@ import { UploadModule } from '~upload/upload.module'
       useFactory: (config: ConfigService) => ({
         introspection: true,
         sortSchema: true,
-        autoSchemaFile: true,
+        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
         engine:
           config.get('env') === 'production'
             ? { reportSchema: true, graphVariant: 'current' }
